90d68a3eb2d45102c04eccec140e9e2dbab223aa
pestbuns <ant.one.dugast@gmail.com>
2018-09-11 22:50:27 +0200

    cleanup code typos

--- src/gol_manager.h
+++ src/gol_manager.h
@@ -3,9 +3,9 @@
 
 
 typedef struct gol {
-    char **map; // map of chars
-    unsigned int width; // map width
-    unsigned int height; // map height
+    char **map;
+    unsigned int width;
+    unsigned int height;
     char tok;
     unsigned int current_cells_nbr;
 } gol_t;
@@ -13,8 +13,10 @@ typedef struct gol {
 
 gol_t *gol_init_map_handler(unsigned int width, unsigned int height);
 void gol_free_handler(gol_t *hdl);
+
 void gol_fill_map(gol_t *hdl, unsigned int nbr, char c);
 void gol_dump_map(gol_t *hdl);
 
 
 #endif /* __GOL_MANAGER_H__ */
+
